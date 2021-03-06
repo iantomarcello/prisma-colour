9ee29c381f9e19de40a3bad1cd03ea010b146e97
iantomarcello <ianyys94@gmail.com>
2021-03-06 14:07:21 +0800

    Fix: Correct path to default import

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import { terser } from 'rollup-plugin-terser';
 export default {
   input: 'src/prisma-colour.js',
   output: [
-    { file: 'build/cjs/prisma-colour.js', format: 'cjs', name: 'Prisma', plugins: [terser()]},
-    { file: 'build/esm/prisma-colour.js', format: 'es', plugins: [terser()] },
+    { file: 'build/cjs/index.js', format: 'cjs', name: 'Prisma', plugins: [terser()]},
+    { file: 'build/esm/index.js', format: 'es', plugins: [terser()] },
   ],
 };
